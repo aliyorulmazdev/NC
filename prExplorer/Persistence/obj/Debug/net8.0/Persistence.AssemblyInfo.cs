011cfebef5202c9202adbfa1ee35bc4e722ecc07
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-08-21 09:57:19 +0300

    IDK

--- prExplorer/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ prExplorer/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b59894a139d96936fb4883daa31b7397a0d7d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152a017be7363e861a12a49c27aec3b1767bfc2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
