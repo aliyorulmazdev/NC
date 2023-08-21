ff5a9e4d21c6faa45217ceb83398ce48c50d3610
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-08-21 13:54:14 +0300

    Initial

--- prExplorer/client-app/src/features/products/dashboard/ProductList.tsx
+++ prExplorer/client-app/src/features/products/dashboard/ProductList.tsx
@@ -1,19 +1,18 @@
 import React, { SyntheticEvent, useState } from "react";
-import { Product } from "../../../app/models/product";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-interface Props {
-  products: Product[];
-  selectProduct: (id: string) => void;
-  deleteProduct: (id: string) => void;
-  submitting: boolean;
-}
+export default observer(function ProductList() {
+  const { productStore } = useStore();
+  const { deleteProduct, productsSortedByName, loading } = productStore;
 
-export default function ProductList({ products, selectProduct, deleteProduct, submitting }: Props) {
+  const [target, setTarget] = useState("");
 
-  const [target, setTarget] = useState('');
-
-  function handleProductDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+  function handleProductDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
     setTarget(e.currentTarget.name);
     deleteProduct(id);
   }
@@ -21,18 +20,33 @@ export default function ProductList({ products, selectProduct, deleteProduct, su
   return (
     <Segment>
       <Item.Group divided>
-        {products.map((product) => (
+        {productsSortedByName.map((product) => (
           <Item key={product.id}>
             <Item.Content>
               <Item.Header as="a">{product.title}</Item.Header>
-              <Item.Meta>Price: {product.price} - Discount : {product.discountPercentage} - Rating: {product.rating}</Item.Meta>
+              <Item.Meta>
+                Price: {product.price} - Discount : {product.discountPercentage}{" "}
+                - Rating: {product.rating}
+              </Item.Meta>
               <Item.Description>
                 <div>{product.description}</div>
                 <div>STOCK: {product.stock}</div>
               </Item.Description>
               <Item.Extra>
-                <Button onClick={() => selectProduct(product.id)} floated="right" content='view' color='blue'/>
-                <Button name = {product.id} loading={submitting && target === product.id} onClick={(e) => handleProductDelete(e, product.id)} floated="right" content='delete' color='red'/>
+                <Button
+                  onClick={() => productStore.selectProduct(product.id)}
+                  floated="right"
+                  content="view"
+                  color="blue"
+                />
+                <Button
+                  name={product.id}
+                  loading={loading && target === product.id}
+                  onClick={(e) => handleProductDelete(e, product.id)}
+                  floated="right"
+                  content="delete"
+                  color="red"
+                />
                 <Label basic content={product.category} />
               </Item.Extra>
             </Item.Content>
@@ -41,4 +55,4 @@ export default function ProductList({ products, selectProduct, deleteProduct, su
       </Item.Group>
     </Segment>
   );
-}
+});
