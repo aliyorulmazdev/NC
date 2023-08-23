d0607a49dff1e01d090f527b6d0a4540e2d77c51
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-08-23 18:12:26 +0300

    react-toastify on client,fluentValidation on back.

--- prExplorer/client-app/src/app/api/agent.ts
+++ prExplorer/client-app/src/app/api/agent.ts
@@ -1,5 +1,8 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Product } from "../models/product";
+import { toast } from "react-toastify";
+import { router } from "../router/Routes";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -9,21 +12,54 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
-axios.interceptors.response.use(async (response) => {
-  try {
-    await sleep(1000);
+axios.interceptors.response.use(
+  async (response) => {
+    await sleep(500);
     return response;
-  } catch (error) {
-    console.log(error);
-    return await Promise.reject(error);
+  },
+  (error: AxiosError) => {
+    const { data, status, config } = error.response as AxiosResponse;
+    switch (status) {
+      case 400:
+        if (config.method === "get" && data.errors.hasOwnProperty("id")) {
+          router.navigate("/not-found");
+        }
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("unauthorized");
+        break;
+      case 403:
+        toast.error("forbidden");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        store.commonStore.setServerError(data);
+        router.navigate("/server-error");
+        break;
+    }
+    return Promise.reject(error);
   }
-});
+);
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
@@ -31,9 +67,10 @@ const requests = {
 const Products = {
   list: () => requests.get<Product[]>("/products"),
   details: (id: string) => requests.get<Product>(`/products/${id}`),
-  create: (product: Product) => axios.post<void>('/products', product),
-  update: (product: Product) => axios.put<void>(`/products/${product.id}`, product),
-  delete: (id: string) => axios.delete<void>(`/products/${id}`)
+  create: (product: Product) => axios.post<void>("/products", product),
+  update: (product: Product) =>
+    axios.put<void>(`/products/${product.id}`, product),
+  delete: (id: string) => axios.delete<void>(`/products/${id}`),
 };
 
 const agent = {
