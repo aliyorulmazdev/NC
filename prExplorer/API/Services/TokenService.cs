9f2477e4005a98c0aebff5162bfd56dd2212ff53
Kübra Yorulmaz <kbr.ercenk@gmail.com>
2023-08-25 16:19:54 +0300

    Initial

---
+++ prExplorer/API/Services/TokenService.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        // Replace "YourSuperSecretKeyHere" with your actual secret key
+        private const string SecretKey = "0000000000000000000000000000000000000000000000000000000000000000"; // 64 bytes
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config)
+        {
+            _config = config;            
+        }
+
+
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- prExplorer/Persistence/Seed.cs
+++ prExplorer/Persistence/Seed.cs
@@ -1,11 +1,26 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Dawid Sibinski", UserName = "Dawid", Email = "dawidsibinski@gmail.com"},
+                    new AppUser{DisplayName = "Ali Yorulmaz", UserName = "Ali", Email = "aliyrlmz@gmail.com"}
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (context.Products.Any()) return;
 
             var products = new List<Product>
