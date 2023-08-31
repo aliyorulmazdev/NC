654434e0e1dc43097e0ff89352dcad49a60cf2cf
aliyorulmazdev <134940699+aliyorulmazdev@users.noreply.github.com>
2023-08-31 14:53:00 +0300

    Solves #16 - App Localization (#23)
    * Initial
    
    * More lang
    
    ---------
    
    Co-authored-by: Kübra Yorulmaz <kbr.ercenk@gmail.com>

--- prExplorer/client-app/src/app/layout/NavBar.tsx
+++ prExplorer/client-app/src/app/layout/NavBar.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import { Button, Container, Dropdown, Image, Menu } from "semantic-ui-react";
 import { Link, NavLink } from "react-router-dom";
 import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import '../../../src/i18n';
+import { useTranslation } from "react-i18next";
 
 export default observer(function NavBar() {
   const {
     userStore: { user, logout },
   } = useStore();
+
+  
+  const {t} = useTranslation();
 
   return (
     <Menu inverted fixed="top">
@@ -20,14 +24,17 @@ export default observer(function NavBar() {
           />
           prExplorer
         </Menu.Item>
-        <Menu.Item name="Products" as={NavLink} to="/products" />
-        <Menu.Item name="Errors" as={NavLink} to="/errors" />
+        <Menu.Item name={t('products')} as={NavLink} to="/products" />
+
+
+
+        <Menu.Item name={t('errors')} as={NavLink} to="/errors" />
         <Menu.Item header>
           <Button
             as={NavLink}
             to="/createProduct"
             positive
-            content="Create Product"
+            content={t('createProduct')}
           />
         </Menu.Item>
         <Menu.Item position="right">
