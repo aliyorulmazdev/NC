59a48d50fb99b6e80667069f1753cf198330abc6
Kübra Yorulmaz <kbr.ercenk@gmail.com>
2023-09-06 09:08:35 +0300

    Added Microsoft.EntityFrameworkCore.Design for mig

---
+++ prExplorer/Persistence/Migrations/20230906060648_ProductCategoryEntity.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductCategoryEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_appUserId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "appUserId",
+                table: "Products",
+                newName: "AppUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_appUserId",
+                table: "Products",
+                newName: "IX_Products_AppUserId");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Thumbnail = table.Column<string>(type: "TEXT", nullable: true),
+                    AppUserId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Categories_AspNetUsers_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_AppUserId",
+                table: "Categories",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_AppUserId",
+                table: "Products",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_AppUserId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "AppUserId",
+                table: "Products",
+                newName: "appUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_AppUserId",
+                table: "Products",
+                newName: "IX_Products_appUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_appUserId",
+                table: "Products",
+                column: "appUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
